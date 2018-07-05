1397618ff4af933057593de1c8a547c541899353
Adibi <maadibi@ead.ubc.ca>
2018-07-05 10:30:55 -0700

    dataset rearranged

--- R/data.r
+++ R/data.r
@@ -3,13 +3,14 @@
 #' @source Simulated
 #' @format A data frame with columns:
 #' \describe{
+#'  \item{ID}{Patient ID}
+#'  \item{x}{Risk score as an example of a marker}
+#'  \item{c1}{First covariate. Binary variable.}
+#'  \item{c2}{Second covariate. Continuous variable.}
+#'  \item{c3}{Third covariate. Continuous variable.}
+#'  \item{tx}{Binary varianble indicating treatment assignment.}
 #'  \item{ln_time}{Log of follow-up}
 #'  \item{events}{Number of events}
-#'  \item{tx}{Binary varianble indicating treatment assignment.}
-#'  \item{c1}{First covariate. Binary variable.}
-#'  \item{c2}{Second covariate. Continuous variable. }
-#'  \item{c3}{Third covariate. Continuous variable.}
-#'  \item{x}{Risk score as an example of a marker}
 #' }
 #' @examples
 #' \dontrun{
